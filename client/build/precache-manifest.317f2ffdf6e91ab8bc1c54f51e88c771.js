5d3b8249167fb590fb5c521619cbd6112e2cb6ce
Robert <primalvinyl@users.noreply.github.com>
2020-04-29 12:28:19 -0700

    adds build directory to git

---
+++ client/build/precache-manifest.317f2ffdf6e91ab8bc1c54f51e88c771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972fa54ef41a8e3e33b0c94232d9d510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "222cfb0cc79bd9261abc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f21b0af0209db92cd698",
+    "url": "/static/js/2.62b8e94d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62b8e94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222cfb0cc79bd9261abc",
+    "url": "/static/js/main.0462b1a8.chunk.js"
+  },
+  {
+    "revision": "1924d03eb4076a12c440",
+    "url": "/static/js/runtime-main.95663480.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

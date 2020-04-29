76d3090a1bb61690069bdab17fcc9aaaf04c3b65
Robert <primalvinyl@users.noreply.github.com>
2020-04-29 12:35:12 -0700

    removes routes

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -16,21 +14,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use('/', indexRouter); 
-//app.use('/users', usersRouter);
-
-
-
-
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
